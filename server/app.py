a12ef615dc5b0c772578f12b96c6bf4c82f517de
Katya Weicht <katya.weicht@gmail.com>
2025-07-25 12:39:40 -0600

    Create endpoints for all events and a particular events sessions

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
 
 from models import db, Event, Session, Speaker, Bio
@@ -18,12 +18,42 @@ db.init_app(app)
 
 @app.route('/events')
 def get_events():
-    pass
+    events = []
+    for event in Event.query.all():
+        event_dict = {
+            "id": event.id,
+            "location": event.location,
+            "name": event.name
+            
+        }
+        events.append(event_dict)
+    
+    body = events
+    status = 200
+    return make_response(body, status)
+        
 
 
 @app.route('/events/<int:id>/sessions')
 def get_event_sessions(id):
-    pass
+    event = Event.query.filter_by(id = id).first()
+    sessions = []
+    if event:
+        for session in event.sessions:
+            session_dict = {
+                "id": session.id,
+                "title": session.title,
+                "start_time": session.start_time
+            }
+            sessions.append(session_dict)
+        body = sessions
+        status = 200
+    else:
+        body = {'message': f'Event {id} not found'}
+        status = 404
+
+    return make_response(body, status)
+
 
 
 @app.route('/speakers')
